8861200d990daee3651c107b5bb8a3b075495c94
Alan Abed <alanabed@Alans-MacBook-Air.local>
2022-06-04 15:26:58 -0700

    added js var questions with all info needed and added css stylings

---
+++ assets/js/script.js
@@ -0,0 +1,23 @@
+var questions = [
+
+    {
+        question: "An array in Java Script is defined as which of the following?", 
+        choices: ["A An ordered list of strings","B An ordered list of values","C A list of loops","D None of the above"],
+        answer: "B An ordered list of values"
+    },
+    {
+        question: "In JS Which of the following is used to make an object into a JSON string?", 
+        choices: ["A int()","B splice()","C stringify()","D All of the above"],
+        answer: "C stringify()"
+    },
+    {
+        question: "html tags are enclosed in?", 
+        choices: ["A &div&","B :div:","C {div}","D <div>"],
+        answer: "D <div>"
+    },
+    {
+        question: "Which of the following are not Java Script primitive values?", 
+        choices: ["A boolean Type","B Objects","C Number Types" ,"D Strings"],
+        answer: "B Objects"
+    }
+]
