44e6b2f03dc94f320ea7400d491e491b51fdc2ab
Alan Abed <alanabed@Alans-MacBook-Air.local>
2022-06-04 15:30:51 -0700

    added variables and logic to quiz start btn, choiceclicked, next question, populate question

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,3 +21,58 @@ var questions = [
         answer: "B Objects"
     }
 ]
+
+var currentQuestion = 0;
+var totalQuestions = questions.length;
+var correct = 0;
+var totalTime = 60;
+var timerInterval = null;
+var score = 0;
+var initials = "";
+
+function choiceClicked(event) {
+    var data = questions[currentQuestion];
+    if (event.target.innerText === data.answer) {
+        document.getElementById("feedback-container").innerText = "You got it right!";
+        document.getElementById("feedback-container").style.color="green";
+        correct++;
+    } else {
+        totalTime = totalTime - 20;
+        document.getElementById("feedback-container").innerText = "You got it wrong";
+        document.getElementById("feedback-container").style.color="red";
+    }
+
+    if (currentQuestion === totalQuestions - 1) {
+        document.getElementById("game-over").innerText = "Game Is Over";
+        clearInterval(timerInterval);
+        document.getElementById("end-of-game").style.display="flex";
+        score = (correct / totalQuestions) * 100;
+        document.getElementById("final-score").innerText = score + "%";
+    } else {
+       currentQuestion++;
+    }
+}
+function nextQuestionClicked() {
+    populateQuestion(currentQuestion);
+    document.getElementById("feedback-container").innerText= "";
+}
+
+function populateQuestion(questionId) {
+    var data = questions[questionId];
+    var questionTag = document.getElementById("question");
+    questionTag.innerHTML = data.question;
+
+    var answerOptionsTag = document.getElementById("answer-options");
+    
+    var choiceButtons = answerOptionsTag.children;
+    for (var i = 0; i < choiceButtons.length; i++) {
+        var btn = choiceButtons[i];
+        btn.innerText = data.choices[i];
+    }
+}
+
+function startButtonClicked(event) {
+    document.getElementById("question-container").style.display="block";
+    event.target.disabled = true;
+    populateQuestion(currentQuestion);
+}
